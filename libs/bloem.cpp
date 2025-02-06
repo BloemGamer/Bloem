bb8890234d16137d9a9adb6aea0eeeea55428a6b
Bloem <sem@ie-design.nl>
2025-02-06 13:44:25 +0100

    it is now possible to use #extra_functions, to make sure you are able to use your own C++ functions

--- libs/bloem.cpp
+++ libs/bloem.cpp
@@ -1,29 +1,35 @@
+#include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <vector>
+#include <functional>
+#include <cstring>
 #include "bloem.hpp"
 
 void bloem::setup(char* filename_)
 {
 	std::size_t amount_lines;
-	// std::size_t commented_lines = 0;
 	std::string text_file;
 	std::string file;
 	filename.replace_filename(filename_);
 	std::ifstream input(filename);
-	// uintmax_t length = std::filesystem::file_size(filename) / sizeof(char);
 	for(amount_lines = 0; std::getline(input, text_file); amount_lines++)
 	{
-		// if(text_file.starts_with((char*)"//"))
-		// {
-		// 	continue;
-		// }
-		// size_t start_comment = text_file.find("//");
-
+		if((std::size_t)text_file.find("#extra_functions") != std::string::npos)
+		{
+			extra_functions = __max(extra_functions, atoll(text_file.c_str() + std::strlen((char*)"#extra_functions ")));
+			continue;
+		}
 		file.append(text_file, 0, text_file.find("//"));
+		file.append("\n");
 
 	}
 
+	
 	input.close();
+	functions.resize(__max(functions.size(), extra_functions + amount_basic_instructions));
+	std::cout << file;
+
 }
 
 void bloem::run()
--- libs/bloem.hpp
+++ libs/bloem.hpp
@@ -1,11 +1,18 @@
+#pragma once
+
 #include <filesystem>
+#include <vector>
+#include <functional>
 
 class bloem
 {
 private:
 	std::filesystem::path filename;
-	/* data */
+	
+	std::size_t extra_functions = 0;
+	std::size_t amount_basic_instructions = 0;
 public:
+	std::vector<std::function<void(void)>> functions;
 	void setup(char* filename);
 	void run();
 
