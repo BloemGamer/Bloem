b03c7fdc840cce28f3b50689427babcaad657285
Bloem <sem@ie-design.nl>
2025-02-06 23:19:53 +0100

    main now shows a little preview, together with test.txt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <filesystem>
+#include "libs/bloem.hpp"
+
+void func0(void);
+void func1(void);
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello world";
+	bloem bloem;
+	bloem.filename.replace_filename(*argv);
+	bloem.filename.remove_filename();
+	bloem.filename.append("test.txt");
+	bloem.setup();
+	bloem.add_function(func1, 1);
+	bloem.add_function(func0, 0);
+	bloem.run();
+}
+
+void func0(void)
+{
+	std::cout << "Hello world (func0)\n";
+}
+
+void func1(void)
+{
+	std::cout << "Hello world (func1)\n";
 }
